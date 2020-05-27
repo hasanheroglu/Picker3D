6b89c8d726bb659209a1c9277e82c6240fa3b579
Hasan Hüsamettin Eroğlu <hasanheroglu@gmail.com>
2020-05-27 20:45:31 +0300

    Added picker, collectibles and collectbox.

---
+++ Assets/Scripts/CollectBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectBox : MonoBehaviour
+{
+    private int collectedCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectedCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            collectedCount++;
+        }
+        
+        Debug.Log("Collectibles collected: " + collectedCount);
+    }
+}
---
+++ Assets/Scripts/PickerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PickerController : MonoBehaviour
+{
+    [SerializeField] private float forwardSpeed = 10f;
+    [SerializeField] private float sidewaySpeed = 1f;
+
+    private bool waitForCheckpoint;
+    private Collider[] _colliders;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitForCheckpoint = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (!waitForCheckpoint)
+        {
+            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, 0f) * sidewaySpeed;
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 1f) * forwardSpeed;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+            _colliders = Physics.OverlapBox(transform.position + GetComponent<BoxCollider>().center, GetComponent<BoxCollider>().size / 2);
+            
+            int count = 0;
+            for (int i = 0; i < _colliders.Length; i++)
+            {
+            
+                if (_colliders[i].CompareTag("Collectible"))
+                {
+                    _colliders[i].GetComponent<Rigidbody>().AddForce(Vector3.forward * 5f);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CollectBox"))
+        {
+            waitForCheckpoint = true;
+        }
+    }
+}
